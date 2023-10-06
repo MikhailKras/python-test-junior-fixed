6a9f880c7f466d693e3f3c3fa80b0f7e46ec6cbe
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-10-06 15:24:43 +0300

    fix: added trailing slash to the path for `api/v1/event/`

--- api/app/src/config/urls.py
+++ api/app/src/config/urls.py
@@ -3,5 +3,5 @@ from src.app.controller import TestController, EventController
 
 urlpatterns = [
     path('test/', TestController.as_view({'get': 'get_result'})),
-    path('api/v1/event', EventController.as_view(), name='event')
+    path('api/v1/event/', EventController.as_view(), name='event')
 ]
